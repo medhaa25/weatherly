2034369f71938dcdcd9d2143b6301919347e4063
medhaa25 <medharwt25.2000@gmail.com>
2024-08-08 17:06:25 +0530

    copyright

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Weather from "./components/Weather";
 
 function App() {
+  const currentYear = new Date().getFullYear();
   return (
     <div className="app">
       <Weather />
+      <p className="copyright">
+        © {currentYear} Medha Rawat. All rights reserved.
+      </p>
     </div>
   );
 }
